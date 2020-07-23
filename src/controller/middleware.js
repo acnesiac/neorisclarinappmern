6ca99b8f452abef7b103dfd95949f8a3ff8da427
acnesiac <acnesiac@hotmail.com>
2020-07-23 18:01:06 -0500

    middleware added

--- src/controller/middleware.js
+++ src/controller/middleware.js
@@ -1,4 +1,4 @@
-const DiaFeriado = require('../models/task');
+const DiaFeriado = require('../models/diaferiado');
 const PgnovasAPI = require('../controller/pgnovas');
 
 const asyncApiCall = async () => {
@@ -7,15 +7,20 @@ const asyncApiCall = async () => {
 module.exports = {
 
     getDiaFeriados: (req,res) =>  {
-        const tasks = DiaFeriado.find();
-        if(tasks.length > 3 ){
-            res.json(tasks);
+        const diasFeriados = DiaFeriado.find();
+        if(diasFeriados.length > 0 ){
+            res.json(diasFeriados);
         }else{
             asyncApiCall().
             then(response => {
                 response.data.forEach(
-                    (value)=> console.log(value)
+                    (dia)=> {
+                        let newDia = {title : dia.id, description: dia.id}
+                        const task = new DiaFeriado(newDia);
+                        task.save();
+                    }
                 );
+                res.json(response.data);
             });
         }
     }
